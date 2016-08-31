e0116fdf74828b69fe90b86c509e2d4878f8944b
chenxz <2163623522@qq.com>
2016-08-31 22:58:39 +0800

    解析数据，绘制人脸识别框

--- how_old/src/main/java/com/cxz/how_old/MainActivity.java
+++ how_old/src/main/java/com/cxz/how_old/MainActivity.java
@@ -4,16 +4,30 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import com.facepp.error.FaceppParseException;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView mPhoto;
     private Button mGetImage;
@@ -24,6 +38,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final int PICK_CODE = 0X110;
     private String mCurrentPhotoStr;
     private Bitmap mPhotoImg;
+    private Paint mPaint;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +51,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         initViews();
-        
+
         initEvents();
 
+        mPaint = new Paint();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     private void initEvents() {
@@ -52,10 +77,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == PICK_CODE){
-            if(data != null){
+        if (requestCode == PICK_CODE) {
+            if (data != null) {
                 Uri uri = data.getData();
-                Cursor cursor = getContentResolver().query(uri,null,null,null,null);
+                Cursor cursor = getContentResolver().query(uri, null, null, null, null);
                 cursor.moveToFirst();
 
                 int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
@@ -77,26 +102,184 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
 
-        BitmapFactory.decodeFile(mCurrentPhotoStr,options);
+        BitmapFactory.decodeFile(mCurrentPhotoStr, options);
 
-        double ratio = Math.max(options.outWidth*1.0d/1024f,options.outHeight*1.0d/1024f);
+        double ratio = Math.max(options.outWidth * 1.0d / 1024f, options.outHeight * 1.0d / 1024f);
 
         options.inSampleSize = (int) Math.ceil(ratio);
         options.inJustDecodeBounds = false;
-        mPhotoImg = BitmapFactory.decodeFile(mCurrentPhotoStr,options);
+        mPhotoImg = BitmapFactory.decodeFile(mCurrentPhotoStr, options);
+    }
+
+    private static final int MSG_SUCCESS = 0x111;
+    private static final int MSG_ERROR = 0X112;
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case MSG_SUCCESS:
+
+                    mWaiting.setVisibility(View.GONE);
+                    JSONObject res = (JSONObject) msg.obj;
+
+                    prepareResBitmap(res);
+
+                    mPhoto.setImageBitmap(mPhotoImg);
+
+                    break;
+                case MSG_ERROR:
+                    mWaiting.setVisibility(View.GONE);
+                    String errorMsg = (String) msg.obj;
+                    if (TextUtils.isEmpty(errorMsg)) {
+                        mTip.setText("Error");
+                    } else {
+                        mTip.setText(errorMsg);
+                    }
+                    break;
+            }
+        }
+    };
+
+    private void prepareResBitmap(JSONObject res) {
+
+        Bitmap bitmap = Bitmap.createBitmap(mPhotoImg.getWidth(), mPhotoImg.getHeight(), mPhotoImg.getConfig());
+        Canvas canvas = new Canvas(bitmap);
+        canvas.drawBitmap(mPhotoImg, 0, 0, null);
+
+        try {
+            JSONArray faces = res.getJSONArray("face");
+            int faceCount = faces.length();
+            mTip.setText("find " + faceCount + " face");
+            for (int i = 0; i < faceCount; i++) {
+                //拿到单独的face对象
+                JSONObject face = faces.getJSONObject(i);
+                JSONObject posObj = face.getJSONObject("position");
+
+                float x = (float) posObj.getJSONObject("center").getDouble("x");
+                float y = (float) posObj.getJSONObject("center").getDouble("y");
+
+                float w = (float) posObj.getDouble("width");
+                float h = (float) posObj.getDouble("height");
+
+                x = x / 100 * bitmap.getWidth();
+                y = y / 100 * bitmap.getHeight();
+
+                mPaint.setColor(0xffffffff);
+                mPaint.setStrokeWidth(3);
+
+                //画box
+                canvas.drawLine(x - w / 2, y - h / 2, x - w / 2, y + h / 2, mPaint);
+                canvas.drawLine(x - w / 2, y - h / 2, x + w / 2, y - h / 2, mPaint);
+                canvas.drawLine(x + w / 2, y - h / 2, x + w / 2, y + h / 2, mPaint);
+                canvas.drawLine(x - w / 2, y + h / 2, x + w / 2, y + h / 2, mPaint);
+
+                //get age and gender
+                int age = face.getJSONObject("attribute").getJSONObject("age").getInt("value");
+                String gender = face.getJSONObject("attribute").getJSONObject("gender").getString("value");
+
+                Bitmap ageBitmap = buildAgeBitmap(age, "Male".equals(gender));
+
+                int ageWidth = ageBitmap.getWidth();
+                int ageHeight = ageBitmap.getHeight();
+
+                //
+
+                mPhotoImg = bitmap;
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Bitmap buildAgeBitmap(int age, boolean isMale) {
+
+        TextView tv = (TextView) mWaiting.findViewById(R.id.id_age_gender);
+        tv.setText(age + "");
+        if (isMale) {
+            tv.setCompoundDrawablesWithIntrinsicBounds(getResources().getDrawable(R.drawable.male), null, null, null);
+        } else {
+            tv.setCompoundDrawablesWithIntrinsicBounds(getResources().getDrawable(R.drawable.female), null, null, null);
+        }
+        tv.setDrawingCacheEnabled(true);
+        Bitmap bitmap = Bitmap.createBitmap(tv.getDrawingCache());
+        tv.destroyDrawingCache();
+
+        return bitmap;
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.id_getImage:
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 intent.setType("image/*");
-                startActivityForResult(intent,PICK_CODE);
+                startActivityForResult(intent, PICK_CODE);
                 break;
 
             case R.id.id_detect:
+
+                FaceppDetect.detect(mPhotoImg, new FaceppDetect.CallBack() {
+
+                    @Override
+                    public void success(JSONObject result) {
+                        Message msg = Message.obtain();
+                        msg.what = MSG_SUCCESS;
+                        msg.obj = result;
+                        mHandler.sendMessage(msg);
+                    }
+
+                    @Override
+                    public void error(FaceppParseException exception) {
+                        Message msg = Message.obtain();
+                        msg.what = MSG_ERROR;
+                        msg.obj = exception;
+                        mHandler.sendMessage(msg);
+                    }
+                });
+
                 break;
         }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.cxz.how_old/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.cxz.how_old/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
